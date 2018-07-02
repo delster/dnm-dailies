adf5727a150a071d311a598e11f69a5d3680d303
David Elster <davidandtheweb@gmail.com>
2018-07-01 23:48:27 -0400

    Countdown scaffolding

---
+++ js/main.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const clockTag = document.querySelector(".countdown");
+  const clockHrsTag = document.querySelector(".countdown__hours");
+  const clockMinsTag = document.querySelector(".countdown__minutes");
+  const clockSecsTag = document.querySelector(".countdown__seconds");
+  const clock = () => {
+    /* initClock */
+  };
+});
